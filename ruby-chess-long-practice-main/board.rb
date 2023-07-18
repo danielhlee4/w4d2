b3ece7c2a2286e4c37620b364f26f2265a342419
Dan Lee <danielhlee4@gmail.com>
2023-07-18 16:43:46 -0400

    edit move_piece

--- ruby-chess-long-practice-main/board.rb
+++ ruby-chess-long-practice-main/board.rb
@@ -2,6 +2,7 @@ class Board
     def initialize
         @board = Array.new(8) { Array.new(8) }
         @piece = Piece.new #placeholder
+        @null_piece = NullPiece.new
     end
 
     def [](pos)
@@ -13,14 +14,20 @@ class Board
         @board[row][col] = val
     end
 
-    def move_piece(start_pos, end_pos)
+    def move_piece(color, start_pos, end_pos)
         # if the board start_pos is null piece or other players piece, raise error
-        if @board[start_pos].is_a?(NullPiece) #did not implement to consider other players piece
+        if @board[start_pos].is_a?(NullPiece) || @board[start_pos].color != color
             raise "error, invalid start position" 
         end
         # if end_pos contains current player's piece
-        if @board[end_pos].is_a?(NullPiece) || @board[end_pos].color != 
-        # if move is succesful, render board to reflect the result
+        if @board[end_pos].is_a?(NullPiece) || @board[end_pos].color != color
+            # if move is succesful, render board to reflect the result
+            @board[start_pos] = @null_piece
+            @board[end_pos] = @piece
+        else
+            raise 'error, invalid end position'
+        end
+        
     end
 end
 
