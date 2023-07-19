0d5c8527db97446759b33e45dc5dd7f91264b37e
tanziti <tanzitian3@gmail.com>
2023-07-19 12:17:31 -0400

    gum gum

--- ruby-chess-long-practice-main/slideable.rb
+++ ruby-chess-long-practice-main/slideable.rb
@@ -11,8 +11,26 @@ module Slideable
         DIAGONAL_DIRS
     end
 
+    # def grow_unblocked_moves_in_dir(dx, dy)
+    #     moves = []
+    #     curr_pos = self.pos
+    #     x, y = curr_pos 
+    #     nxt_pos = [x + dx, y + dy]
+    #     while nxt_pos.valid? 
+    #      x, y = curr_pos 
+    #      nxt_pos = [x + dx, y + dy]
+    #       if nxt_pos.valid?   #if nxt_pos is valid, add it to the array of pos
+    #         moves << nxt_pos 
+    #       end
+    #      curr_pos = nxt_poss
+    #     end
+    #     moves
+    # end
+
     def grow_unblocked_moves_in_dir(dx, dy)
-        x, y = self.pos
-        
+        moves = []
+        curr_pos = self.pos
+        x, y = curr_pos 
+
     end
 end
