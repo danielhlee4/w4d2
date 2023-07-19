926c6e9e4488560c60c2fe9a7fbd3ee6fa9a293f
Dan Lee <danielhlee4@gmail.com>
2023-07-19 14:20:00 -0400

    add #inside_board? #moves #gum

--- ruby-chess-long-practice-main/slideable.rb
+++ ruby-chess-long-practice-main/slideable.rb
@@ -31,6 +31,27 @@ module Slideable
         moves = []
         curr_pos = self.pos
         x, y = curr_pos 
+        # next_pos = [dx + x, dy + y]
+        next_x = dx + x
+        next_y = dy + y
+        until !@board[next_x][next_y].empty? || !inside_board?(next_x, next_y)
 
+        end
+
+    end
+
+    def inside_board?(row, col)
+        return false if row < 0 || row > 7
+        return false if col < 0 || col > 7
+        true
+    end
+
+    def moves(move_dirs) # move_dirs is an array of 4 directions
+        poss_moves = []
+        move_dirs.each do |dir|
+            row, col = dir
+            poss_moves += grow_unblocked_moves_in_dir(row, col)
+        end
+        poss_moves
     end
 end
